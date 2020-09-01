b7b92ce5b20d61b66f39bde6fae6f2eac4ff12aa
siamsaleh <siamsaleh9@gmail.com>
2020-09-02 00:06:21 +0600

    Firebase UI

---
+++ app/src/main/java/com/example/mediseba/Activity/DoctorCodeActivity.java
@@ -0,0 +1,16 @@
+package com.example.mediseba.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mediseba.R;
+
+public class DoctorCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_code);
+    }
+}
