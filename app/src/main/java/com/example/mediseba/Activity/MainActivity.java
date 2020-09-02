c379aec0d12f5cae9f4c312972713500d821a349
siamsaleh <siamsaleh9@gmail.com>
2020-09-02 16:41:24 +0600

    UI almost Done

--- app/src/main/java/com/example/mediseba/Activity/MainActivity.java
+++ app/src/main/java/com/example/mediseba/Activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.mediseba.Adapter.HomeGridAdapter;
@@ -15,41 +16,99 @@ import com.example.mediseba.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button ambulanceButton;
-    private GridView gridView;
-    private int [] pics = {R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
-    private String[] items;
+//    private Button ambulanceButton;
+//    private GridView gridView;
+//    private int [] pics = {R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
+//    private String[] items;
+
+
+    private Button doctorButton, appointmentButton, medicine_button, ambulance_button, homeCheckUpButton, disTestButton;
+    private ImageView callCenterImage, emergencyCallButton, homeImage, profileImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gridView = findViewById(R.id.gridView_HomePage);
-        items = getResources().getStringArray(R.array.gridView_homePage);
+//        gridView = findViewById(R.id.gridView_HomePage);
+//        items = getResources().getStringArray(R.array.gridView_homePage);
+//
+//        HomeGridAdapter adapter = new HomeGridAdapter(this, items, pics);
+//        gridView.setAdapter(adapter);
+//
+//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String nameOfType = items[position];
+////                Toast.makeText(MainActivity.this, nameOfType, Toast.LENGTH_SHORT).show();
+//                if (nameOfType.equals("Corona Virus")){
+//                    Intent intent = new Intent(MainActivity.this, DoctorListActivity.class);
+//                    intent.putExtra("doctorType", nameOfType);
+//                    startActivity(intent);
+//                }
+//            }
+//        });
 
-        HomeGridAdapter adapter = new HomeGridAdapter(this, items, pics);
-        gridView.setAdapter(adapter);
+//        findViewById(R.id.button_ambulance).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MainActivity.this,AmbulanceActivity.class));
+//            }
+//        });
 
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String nameOfType = items[position];
-//                Toast.makeText(MainActivity.this, nameOfType, Toast.LENGTH_SHORT).show();
-                if (nameOfType.equals("Corona Virus")){
-                    Intent intent = new Intent(MainActivity.this, DoctorListActivity.class);
-                    intent.putExtra("doctorType", nameOfType);
-                    startActivity(intent);
-                }
-            }
-        });
+        doctorButton = findViewById(R.id.home_doctor_Button);
+        appointmentButton = findViewById(R.id.home_appointment_Button);
+        ambulance_button = findViewById(R.id.home_ambulance_Button);
+        medicine_button = findViewById(R.id.home_medicine_Button);
+        homeCheckUpButton = findViewById(R.id.home_check_up_Button);
+        disTestButton = findViewById(R.id.home_dis_test_Button);
 
-        findViewById(R.id.button_ambulance).setOnClickListener(new View.OnClickListener() {
+        doctorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,AmbulanceActivity.class));
+                startActivity(new Intent(MainActivity.this, DoctorCetegoryActivity.class));
             }
         });
 
+        appointmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DoctorCetegoryActivity.class));
+            }
+        });
+
+        ambulance_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AmbulanceActivity.class));
+            }
+        });
+
+        medicine_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DoctorCetegoryActivity.class));
+            }
+        });
+
+        homeCheckUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DoctorCetegoryActivity.class));
+            }
+        });
+
+        disTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DoctorCetegoryActivity.class));
+            }
+        });
+
+
+
+
     }
+
 }
