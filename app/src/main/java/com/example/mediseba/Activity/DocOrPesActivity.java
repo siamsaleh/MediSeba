e2f48a8addd7550b7a332b2fc71faffc0be7833e
siamsaleh <siamsaleh9@gmail.com>
2020-09-02 17:39:51 +0600

    Doc Pas

--- app/src/main/java/com/example/mediseba/Activity/DocOrPesActivity.java
+++ app/src/main/java/com/example/mediseba/Activity/DocOrPesActivity.java
@@ -2,15 +2,53 @@ package com.example.mediseba.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.mediseba.R;
 
 public class DocOrPesActivity extends AppCompatActivity {
 
+    RadioGroup radioGroup;
+    RadioButton radioButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc_or_pes);
+
+        radioGroup = findViewById(R.id.radioGroup);
+
+
+
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int radioId = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(radioId);
+                radioButton.getText();
+                Toast.makeText(DocOrPesActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
+                if (radioButton.getText().equals("Patient")){
+                    startActivity(new Intent(DocOrPesActivity.this, LogInActivity.class));
+                }
+                else {
+                    startActivity(new Intent(DocOrPesActivity.this, DocCodeActivity.class));
+                }
+
+            }
+        });
+
     }
+
+
+    public void checkButton(View v) {
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+//        Toast.makeText(this, "Selected Radio Button: " + radioButton.getText(),
+//                Toast.LENGTH_SHORT).show();
+    }
+
 }
--- app/src/main/java/com/example/mediseba/Activity/DoctorCetegoryActivity.java
+++ app/src/main/java/com/example/mediseba/Activity/DoctorCetegoryActivity.java
@@ -2,7 +2,9 @@ package com.example.mediseba.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.mediseba.R;
 
@@ -12,5 +14,129 @@ public class DoctorCetegoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_cetegory);
+
+        findViewById(R.id.covid_doctor_Button).setOnClickListener(new View.OnClickListener() {         //covid
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorCetegoryActivity.this, DoctorListActivity.class);
+                intent.putExtra("doctorType", "covid");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.skin_doctor_Button).setOnClickListener(new View.OnClickListener() {         //skin
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorCetegoryActivity.this, DoctorListActivity.class);
+                intent.putExtra("doctorType", "skin");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.medicine_doctor_Button).setOnClickListener(new View.OnClickListener() {      //medicine
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorCetegoryActivity.this, DoctorListActivity.class);
+                intent.putExtra("doctorType", "medicine");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.surgery_doctor_Button).setOnClickListener(new View.OnClickListener() {      //surgery
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorCetegoryActivity.this, DoctorListActivity.class);
+                intent.putExtra("doctorType", "surgery");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.eye_doctor_Button).setOnClickListener(new View.OnClickListener() {        //eye
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorCetegoryActivity.this, DoctorListActivity.class);
+                intent.putExtra("doctorType", "eye");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.dentist_doctor_Button).setOnClickListener(new View.OnClickListener() {    //dentist
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorCetegoryActivity.this, DoctorListActivity.class);
+                intent.putExtra("doctorType", "dentist");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.chest_doctor_Button).setOnClickListener(new View.OnClickListener() {      //chest
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorCetegoryActivity.this, DoctorListActivity.class);
+                intent.putExtra("doctorType", "chest");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.brain_doctor_Button).setOnClickListener(new View.OnClickListener() {        //brain
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorCetegoryActivity.this, DoctorListActivity.class);
+                intent.putExtra("doctorType", "brain");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.bones_doctor_Button).setOnClickListener(new View.OnClickListener() {      //bones
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorCetegoryActivity.this, DoctorListActivity.class);
+                intent.putExtra("doctorType", "bones");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.child_doctor_Button).setOnClickListener(new View.OnClickListener() {      //child
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoctorCetegoryActivity.this, DoctorListActivity.class);
+                intent.putExtra("doctorType", "child");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.backButton_doctor_category).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        findViewById(R.id.homeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DoctorCetegoryActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+//
+//        findViewById(R.id.covid_doctor_Button).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//
+//        findViewById(R.id.covid_doctor_Button).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
